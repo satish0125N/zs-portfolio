83c78fe3db76908e6f9068c1d62f4e4cd3aa7a38
Satish zala <satish@glopbe.com>
2025-01-21 13:11:02 +0530

    about sections change

--- js/header.js
+++ js/header.js
@@ -1,54 +1,56 @@
-import $ from 'jquery';
-import { gsap } from 'gsap';
-import ScrollTrigger from 'gsap/dist/ScrollTrigger';
-import ScrollSmoother from 'gsap/dist/ScrollSmoother';
-import { TweenMax } from 'gsap/gsap-core';
-import { Power1 } from 'gsap';
+import $ from "jquery";
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import ScrollSmoother from "gsap/dist/ScrollSmoother";
+import { TweenMax } from "gsap/gsap-core";
+import { Power1 } from "gsap";
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, TweenMax);
 
 let winW = $(window).width();
 $(window)
-	.bind('resize', function () {
-		if ($(this).width() < 992) {
-			$('.main-menu').addClass('disable');
-		} else {
-			$('.main-menu').removeClass('disable');
-		}
-	})
-	.trigger('resize');
+  .bind("resize", function () {
+    if ($(this).width() < 992) {
+      $(".main-menu").addClass("disable");
+    } else {
+      $(".main-menu").removeClass("disable");
+    }
+  })
+  .trigger("resize");
 
-$('.menu-btn').on('click', () => {
-	$('.menu-btn').toggleClass('active');
-	$('.main-menu').toggleClass('active');
+$(".menu-btn").on("click", () => {
+  $(".menu-btn").toggleClass("active");
+  $(".main-menu").toggleClass("active");
 
-	if ($('.main-menu').hasClass('active')) {
-		$('body').addClass('stop');
-		// $(window).on('scroll');
-		// window.scrollTo(0, 0);
-	} else {
-		$('body').removeClass('stop');
-		// $(window).off('scroll');
-	}
+  if ($(".main-menu").hasClass("active")) {
+    $("body").addClass("stop");
+    // $(window).on('scroll');
+    // window.scrollTo(0, 0);
+  } else {
+    $("body").removeClass("stop");
+    // $(window).off('scroll');
+  }
 
-	if (winW > 992) {
-		$('.main-menu').toggleClass('enable');
-		gsap.from('.main-menu', {
-			duration: 1,
-			scale: 0,
-			ease: 'expoScale(5, 1)',
-		});
-		gsap.to('.main-menu', {
-			duration: 1,
-			scale: 1,
-			ease: 'expoScale(0, 0)',
-		});
-	}
+  if (winW > 992) {
+    $(".main-menu").toggleClass("enable");
+    gsap.from(".main-menu", {
+      duration: 1,
+      opacity: 0,
+      // scale: 0,
+      // ease: 'expoScale(5, 1)',
+    });
+    gsap.to(".main-menu", {
+      duration: 1,
+      opacity: 1,
+      // scale: 1,
+      // ease: 'expoScale(0, 0)',
+    });
+  }
 });
 
-$(window).on('scroll', (e) => {
-	if ($(e.currentTarget).scrollTop() > 50) {
-		$('.header').addClass('sticky');
-	} else {
-		$('.header').removeClass('sticky');
-	}
+$(window).on("scroll", (e) => {
+  if ($(e.currentTarget).scrollTop() > 50) {
+    $(".header").addClass("sticky");
+  } else {
+    $(".header").removeClass("sticky");
+  }
 });
